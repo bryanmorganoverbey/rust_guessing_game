73ad0b583fb82a1402cf646581c681f9d253f9fb
Bryan Overbey <bryanmorganoverbey@gmail.com>
2024-02-23 09:49:16 -0600

    remove debugging comments

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
     loop {
         println!("Please input your guess.");
 
@@ -16,7 +15,11 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Ignoring a non-number guess and asking for another guess instead of crashing the program
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
         match guess.cmp(&secret_number) {
